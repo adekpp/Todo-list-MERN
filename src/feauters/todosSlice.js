ca0e18190e3b232666d876fcfc3b8e00b8832a57
adekpp <adekpp@gmail.com>
2022-06-19 15:13:02 +0200

    provided api access

--- src/feauters/todosSlice.js
+++ src/feauters/todosSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API = "http://localhost:5000/todos";
+const API = process.env.REACT_APP_API
 
 export const getTodos = createAsyncThunk("todos/getTodos", async () => {
   const response = await axios.get(API);
